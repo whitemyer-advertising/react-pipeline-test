3f2ee65d51322426b76b347de9683862d77954d6
whitemyer-advertising <webdev1@whitemyer.com>
2019-04-24 18:20:22 +0000

    Removed build dir from gitignore

---
+++ build/precache-manifest.3637c6561f46b7a7d46811d6dc825d62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c197811c0e60ddefa1c9fa24e714610",
+    "url": "/react-pipeline-coaching/index.html"
+  },
+  {
+    "revision": "383926d072fd0da41812",
+    "url": "/react-pipeline-coaching/static/css/main.3ab2cb51.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-pipeline-coaching/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "383926d072fd0da41812",
+    "url": "/react-pipeline-coaching/static/js/main.14d0d1cd.chunk.js"
+  },
+  {
+    "revision": "bc4316a3ff6a62c7371d",
+    "url": "/react-pipeline-coaching/static/js/runtime~main.e887e9a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pipeline-coaching/static/media/logo.5d5d9eef.svg"
+  }
+]);
